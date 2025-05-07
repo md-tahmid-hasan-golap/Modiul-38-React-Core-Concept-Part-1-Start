d4018a1479019a24fea99858149a8dd813745305
md-tahmid-hasan-golap <golapraj47@gmail.com>
2025-05-07 12:27:15 +0600

    resolve

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>React core concept</h1>
+      <Student></Student>
+      <Student></Student>
     </>
-  )
+  );
 }
 
-export default App
+function Person() {
+  const age = 21;
+  return <p>I am a person {age}</p>;
+}
+
+function Student() {
+  return (
+    <div className="student">
+      <p className="p">Name :</p>
+      <p>Dept :</p>
+    </div>
+  );
+}
+
+function Sports() {
+  const play = "Cricket";
+  return (
+    <div>
+      <h3>{play}</h3>
+      <p>Playing & losing</p>
+    </div>
+  );
+}
+
+export default App;
